b965c35f1f7c335bd5a933fd14bbac07d1c92298
Abhishek <abhi.una12@gmail.com>
2019-05-30 11:32:27 +0530

    Remove redundant options

--- scripts/bash/install_opencv.sh
+++ scripts/bash/install_opencv.sh
@@ -13,11 +13,11 @@ echo "Installing OpenCV Dependencies..."
 
 sudo apt-get install -y build-essential cmake unzip wget pkg-config gfortran
 
-sudo apt-get install -y yasm libv4l-dev qt5-default libvtk6-dev 
+sudo apt-get install -y yasm libv4l-dev python2-dev libgtk-3-dev
 
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libjasper-dev libopenexr-dev
 
-sudo apt-get install -y libxvidcore-dev libx264-dev libatlas-base-dev libtiff5-dev 
+sudo apt-get install -y libxvidcore-dev libx264-dev libatlas-base-dev libtiff5-dev python3-dev
 
 sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libopenblas-base
 
@@ -43,7 +43,7 @@ cd $HOME/opencv
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DOPENCV_EXTRA_MODULES_PATH=$HOME/opencv_contrib/modules -DCMAKE_INSTALL_PREFIX=/usr/local -DINSTALL_PYTHON_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_EXAMPLES=OFF  -DBUILD_TESTS=OFF  -DWITH_LIBV4L=0N  -DWITH_V4L=ON -DOPENCV_SKIP_PYTHON_LOADER=ON -DWITH_QT=ON ..
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DOPENCV_EXTRA_MODULES_PATH=$HOME/opencv_contrib/modules -DCMAKE_INSTALL_PREFIX=/usr/local -DINSTALL_PYTHON_EXAMPLES=OFF -DBUILD_DOCS=OFF -DBUILD_EXAMPLES=OFF  -DBUILD_TESTS=OFF  -DWITH_LIBV4L=0N  -DWITH_V4L=ON -DOPENCV_SKIP_PYTHON_LOADER=ON ..
 make -j6
 sudo make install
 sudo ldconfig
