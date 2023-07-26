182ca412d607a8f2258f66b077bfacb08fc6f722
abhiTronix <abhi.una12@gmail.com>
2023-07-27 00:47:47 +0530

    💚 CI: Extended fix to more tests.
    - Temporary fix for AST constructor depth mismatch in pytest on python 3.11.x, More information: https://github.com/pytest-dev/pytest/issues/10874

--- vidgear/tests/streamer_tests/asyncio_tests/test_webgear_rtc.py
+++ vidgear/tests/streamer_tests/asyncio_tests/test_webgear_rtc.py
@@ -466,6 +466,10 @@ test_data_class = [
 ]
 
 
+@pytest.mark.skipif(
+    platform.python_version_tuple()[:2] >= ("3", "11"),
+    reason="Random Failures!",
+)
 @pytest.mark.asyncio
 @pytest.mark.parametrize("middleware, result", test_data_class)
 async def test_webgear_rtc_custom_middleware(middleware, result):
