bba3211731ba8b1002fb1cb3f94e2f79680e9e7f
abhiTronix <abhi.una12@gmail.com>
2024-06-24 00:33:55 +0530

    🐛 Helper: Fixed regex expression bugs in `get_supported_demuxers` method.
    - ⚡️ Simplified regex search for demuxers and handled multi-line output correctly.
    - 🎨 Improved output extraction and stripping logic to handle commas within demuxer names.

--- vidgear/gears/helper.py
+++ vidgear/gears/helper.py
@@ -544,12 +544,11 @@ def get_supported_demuxers(path):
     splitted = [x.decode("utf-8").strip() for x in demuxers.split(b"\n")]
     split_index = [idx for idx, s in enumerate(splitted) if "--" in s][0]
     supported_demuxers = splitted[split_index + 1 : len(splitted) - 1]
-    # compile regex
-    finder = re.compile(r"\s\s[a-z0-9_,-]+\s+")
-    # find all outputs
-    outputs = finder.findall("\n".join(supported_demuxers))
-    # return output findings
-    return [o.strip() for o in outputs]
+    # search all demuxers
+    outputs = [re.search(r"\s[a-z0-9_,-]{2,}\s", d) for d in supported_demuxers]
+    outputs = [o.group(0) for o in outputs if o]
+    # return demuxers output
+    return [o.strip() if not ("," in o) else o.split(",")[-1].strip() for o in outputs]
 
 
 def get_supported_pixfmts(path):
