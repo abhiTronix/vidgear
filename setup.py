d27873c468531758a338873c9face2043586f246
abhiTronix <abhi.una12@gmail.com>
2025-03-14 15:28:30 +0530

    🔧 Refactor: Replace pkg_resources with packaging.version for backward compatibility

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import json
 import platform
 import urllib.request
 
-from packaging.version import parse
+from pkg_resources import parse_version
 from distutils.util import convert_path
 from setuptools import setup
 
@@ -39,10 +39,10 @@ def test_opencv():
         import cv2
 
         # check whether OpenCV Binaries are 3.x+
-        if parse(cv2.__version__) < parse("3"):
+        if parse_version(cv2.__version__) < parse_version("3"):
             raise ImportError(
                 "Incompatible (< 3.0) OpenCV version-{} Installation found on this machine!".format(
-                    parse(cv2.__version__)
+                    parse_version(cv2.__version__)
                 )
             )
     except ImportError:
@@ -63,7 +63,7 @@ def latest_version(package_name):
         )
         data = json.load(response)
         versions = list(data["releases"].keys())
-        versions.sort(key=parse)
+        versions.sort(key=parse_version)
         return ">={}".format(versions[-1])
     except Exception as e:
         if versions:
